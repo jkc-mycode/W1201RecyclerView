1a29a35b7e8f19949737c37d398b2701472ca150
jeongchan12 <jkc9709@hanamail.net>
2022-11-23 15:31:20 +0900

    LsitViewModel 수정

--- app/src/main/java/kr/ac/kumoh/s20201414/w1201recyclerview/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w1201recyclerview/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,12 +14,15 @@ import kr.ac.kumoh.s20201414.w1201recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val model:ListViewModel by viewModels()
+    //private val model:ListViewModel by viewModels()
+    private lateinit var model: ListViewModel
     private val songAdapter = SongAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        model = ViewModelProvider(this)[ListViewModel::class.java]
 
         binding.list.apply {
             layoutManager = LinearLayoutManager(applicationContext) //최소한 필요
@@ -28,11 +31,12 @@ class MainActivity : AppCompatActivity() {
             adapter = songAdapter //최소한 필요
         }
 
-        model.getList().observe(this){
-            songAdapter.notifyDataSetChanged()
+        model.list.observe(this){
+            //songAdapter.notifyDataSetChanged()
+            songAdapter.notifyItemRangeChanged(0, model.list.value?.size ?: 0)
         }
 
-        for(i in 1..3) {
+        for(i in 1..30) {
             model.add("소주 한 잔")
         }
         model.add("It's my Life")
@@ -50,9 +54,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.txSong.text = model.getSong(position)
+            holder.txSong.text = model.list.value?.get(position) ?: ""
         }
 
-        override fun getItemCount() = model.getSize()
+        override fun getItemCount() = model.list.value?.size ?: 0
     }
 }
